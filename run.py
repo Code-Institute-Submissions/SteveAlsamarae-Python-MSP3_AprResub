e7be34d1c92f5ac76b8ab106ef8a77d674e8c777
Steve Alsamarae <sarsaif@yahoo.com>
2021-08-26 12:17:27 +0000

    Add quiz questions and their if statements

--- run.py
+++ run.py
@@ -6,3 +6,39 @@ if playing != "yes":
     quit()
 
 print("Okay! Let's play :)")    
+
+
+answer = input("Who is the founder of Apple? ")
+if answer == "Steve Jobs":
+    print('Correct!')
+
+else:
+    print("Incorrect!")
+
+answer = input("Who is the founder of Microsoft? ")
+if answer == "Bill Gates":
+    print('Correct!')
+
+else:
+    print("Incorrect!")
+
+answer = input("Who is the founder of Google? ")
+if answer == "Larry Page":
+    print('Correct!')
+
+else:
+    print("Incorrect!")
+
+answer = input("Who is the founder of Facebook? ")
+if answer == "Mark Zuckerberg":
+    print('Correct!')
+
+else:
+    print("Incorrect!")
+
+answer = input("Who is the founder of Tesla? ")
+if answer == "Elon Musk":
+    print('Correct!')
+
+else:
+    print("Incorrect!")                   
