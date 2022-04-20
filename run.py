dd959d0a0ff0cca597958fa5af5cde2eb208b5f2
Steve Alsamarae <82416290+SteveAlsamarae@users.noreply.github.com>
2022-04-21 00:03:01 +0600

    Add user feedback, handle blank input and add typings

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 
 
-QUESTIONS = [
+QUESTIONS: list[str] = [
     "1. Who is the founder of Apple?\n",
     "2. Who is the founder of Microsoft?\n",
     "3. Who is the founder of Google?\n",
@@ -9,7 +9,7 @@ QUESTIONS = [
     "5. Who is the founder of Tesla?\n",
     ]
 
-ANSWERS = [
+ANSWERS: list[str] = [
     "steve jobs",
     "bill gates",
     "larry page",
@@ -18,12 +18,30 @@ ANSWERS = [
 ]
 
 
-def ask_question(question, default_answer):
+def ask_question(question: str, default_answer: str) -> bool:
+    """Ask a question and return True if the answer is correct and False if not.
+
+    Args:
+        question (str): The question to ask.
+        default_answer (str): The answer to the question.
+
+    Returns:
+        bool: True if the answer is correct and False if not.
+    """
+
     try:
-        answer = input(question)
-    except Exception:
+        answer: str = input(question)
+    except (ValueError, TypeError):
         print("An Error happened during Entering an answer!")
         sys.exit()
+
+    while not answer:
+        try:
+            print("You did not enter an answer! Try entering an answer!")
+            answer: str = input(question)
+        except (ValueError, TypeError):
+            print("An Error happened during Entering an answer!")
+            sys.exit()
 
     if answer.lower() == default_answer:
         print("Correct!")
@@ -35,36 +53,42 @@ def ask_question(question, default_answer):
     return 0
 
 
-def main():
+def main() -> None:
+    """The main function that runs the quiz."""
 
     print("Welcome to my computer quiz!")
 
-    try:
-        playing = input("Do you want to play?\n")
-    except Exception:
-        print("An Error happened during Entering an answer!")
+    while True:
+        try:
+            playing: str = input("Do you want to play?[y/yes]\n")
+        except (ValueError, TypeError):
+            print("An Error occurred during confirmation!")
+
+            sys.exit()
+
+        if not playing:
+            print("You didn't enter an anything!\n")
+            continue
+        break
+
+    if playing.lower() != "y" and playing.lower() != "yes":
+        print("No problem! You can always play later!")
+        print("Just click on 'RUN PROGRAM' button to play again!")
+        print()
         sys.exit()
 
-    if playing.lower() != "yes":
+    print("Let's start the quiz!")
 
-        sys.exit()
+    score: int = 0
+    total_questions: int = len(QUESTIONS)
 
-    print("Okay! Let's play :)")
-
-    score = 0
-
-    total_questions = len(QUESTIONS)
-
-    try:
-        for i in range(total_questions):
-            score += ask_question(QUESTIONS[i], ANSWERS[i])
-    except Exception:
-        print("An Error happened while checking your answer!")
-        sys.exit()
+    for i in range(total_questions):
+        score += ask_question(QUESTIONS[i], ANSWERS[i])
 
     print("You got " + str(score) + " questions correct!")
-
     print("You got " + str((score / total_questions) * 100) + "%")
+
+    sys.exit()
 
 
 if __name__ == "__main__":
